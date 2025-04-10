0dc483148331563920b844195f2a5719a4563196
MegaEmme <mikmusco@gmail.com>
2025-04-10 13:54:02 +0200

    feat: create select and map.movies

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,30 @@ function App() {
   return (
     <>
       {console.log(movies)}
+      <h1>Movie Filter</h1>
+      <section>
+        <h2>Cerca Articoli</h2>
+        <label>Cerca per genere</label>
+        <div>
+          <select name="" id="">
+            <option value="">---</option>
+            <option>Fantascienza</option>
+            <option>Thriller</option>
+            <option>Romantico</option>
+            <option>Azione</option>
+          </select>
+        </div>
+      </section>
+
+      <section>
+        <h2>Lista Film</h2>
+        {movies.map(element =>
+          <article>
+            <h3>Nome:{element.title}</h3>
+            <h4>Genere:{element.genre}</h4>
+          </article>
+        )}
+      </section>
     </>
   )
 }
